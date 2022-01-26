4b0676609aa0ed8062fb31d77db0bbcc437c15b2
sheisdeyandex <sheisde@yandex.com>
2022-01-26 09:32:59 +0500

    up

---
+++ app/src/main/java/com/fast/cleaneral/interfaces/LoadAdmob.java
@@ -0,0 +1,8 @@
+package com.fast.cleaneral.interfaces;
+
+import com.google.android.gms.ads.AdView;
+
+public interface LoadAdmob {
+    void loadAd(AdView adView);
+    void loadInter();
+}
---
+++ app/src/main/java/com/fast/cleaneral/interfaces/purchaseinterface.java
@@ -0,0 +1,5 @@
+package com.fast.cleaneral.interfaces;
+
+public interface purchaseinterface {
+    void subscribe();
+}
